fd64d5f46fb15ddd4637042be07e8136e223c420
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-18 00:35:22 -0300

    Album component and styles

---
+++ components/Album/index.tsx
@@ -0,0 +1,15 @@
+import {Image, Text, View} from 'react-native';
+import styles from './styles';
+
+export type AlbumProps = {
+  album: {id: string; imageUri: string; artistsHeadline: string};
+};
+
+const Album = (props: AlbumProps) => (
+  <View style={styles.container}>
+    <Image source={{uri: props.album.imageUri}} style={styles.image} />
+    <Text style={styles.text}>{props.album.artistsHeadline}</Text>
+  </View>
+);
+
+export default Album;
