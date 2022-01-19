865c104b52dff188d7517ba1ace14d19972017dd
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-19 19:25:48 -0300

    Style and variable names changes

--- components/Album/index.tsx
+++ components/Album/index.tsx
@@ -1,14 +1,17 @@
 import {Image, Text, View} from 'react-native';
+import {AlbumType} from '../../types';
 import styles from './styles';
 
 export type AlbumProps = {
-  album: {id: string; imageUri: string; artistsHeadline: string};
+  album: AlbumType;
 };
 
 const Album = (props: AlbumProps) => (
   <View style={styles.container}>
     <Image source={{uri: props.album.imageUri}} style={styles.image} />
-    <Text style={styles.text}>{props.album.artistsHeadline}</Text>
+    <Text numberOfLines={2} style={styles.text}>
+      {props.album.artistsHeadline}
+    </Text>
   </View>
 );
 
