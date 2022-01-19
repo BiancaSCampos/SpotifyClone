dc7f1092ecc5752d134c1657820ac40f3a135343
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-19 19:27:54 -0300

    Data and variable names changes

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,17 +1,34 @@
 import {Text, View} from 'react-native';
-import Album from '../../components/Album';
+import AlbumCategory from '../../components/AlbumCategory';
 import {RootTabScreenProps} from '../../types';
 
-const album = {
+const albumCategory = {
   id: '1',
-  imageUri: 'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
-  artistsHeadline: 'Taylor Swift, Ed Sheeran, Snoop Dogg',
+  title: 'Happy Vibes',
+  albums: [
+    {
+      id: '1',
+      imageUri: 'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
+      artistsHeadline: 'Taylor Swift, Ed Sheeran, Snoop Dogg',
+    },
+    {
+      id: '2',
+      imageUri: 'https://upload.wikimedia.org/wikipedia/pt/c/cf/Lady_Gaga_-_Joanne.jpg',
+      artistsHeadline: 'Lady Gaga, Ed Sheeran, Snoop Dogg',
+    },
+    {
+      id: '3',
+      imageUri: 'https://m.media-amazon.com/images/I/71nenZdrRkL._AC_SY355_.jpg',
+      artistsHeadline: 'Taylor Swift, Ed Sheeran, Snoop Dogg',
+    },
+  ],
 };
 
 export default function HomeScreen({navigation}: RootTabScreenProps<'Home'>) {
   return (
     <View>
-      <Album album={album} />
+      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
+      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
     </View>
   );
 }
