f09abcac19a48da10b2fd4325e644c642e8e042c
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-19 19:28:04 -0300

    AlbumType added

--- types.tsx
+++ types.tsx
@@ -32,3 +32,9 @@ export type RootTabScreenProps<Screen extends keyof RootTabParamList> = Composit
   BottomTabScreenProps<RootTabParamList, Screen>,
   NativeStackScreenProps<RootStackParamList>
 >;
+
+export type AlbumType = {
+  id: string;
+  imageUri: string;
+  artistsHeadline: string;
+};
