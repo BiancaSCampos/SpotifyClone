97b80fa32b00804bb8fdad101284219f6c09ea49
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-20 11:24:56 -0300

    FlatList added to code to display a list of album categories

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,34 +1,18 @@
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import AlbumCategory from '../../components/AlbumCategory';
 import {RootTabScreenProps} from '../../types';
-
-const albumCategory = {
-  id: '1',
-  title: 'Happy Vibes',
-  albums: [
-    {
-      id: '1',
-      imageUri: 'https://i.pinimg.com/originals/3a/f0/e5/3af0e55ea66ea69e35145fb108b4a636.jpg',
-      artistsHeadline: 'Taylor Swift, Ed Sheeran, Snoop Dogg',
-    },
-    {
-      id: '2',
-      imageUri: 'https://upload.wikimedia.org/wikipedia/pt/c/cf/Lady_Gaga_-_Joanne.jpg',
-      artistsHeadline: 'Lady Gaga, Ed Sheeran, Snoop Dogg',
-    },
-    {
-      id: '3',
-      imageUri: 'https://m.media-amazon.com/images/I/71nenZdrRkL._AC_SY355_.jpg',
-      artistsHeadline: 'Taylor Swift, Ed Sheeran, Snoop Dogg',
-    },
-  ],
-};
+import albumCategories from '../../data/albumCategories';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function HomeScreen({navigation}: RootTabScreenProps<'Home'>) {
   return (
-    <View>
-      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
-      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
-    </View>
+    <LinearGradient colors={['#8FF4E2', '#000000']} start={[-2, 0]}>
+      <FlatList
+        data={albumCategories}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => <AlbumCategory title={item.title} albums={item.albums} />}
+      />
+    </LinearGradient>
   );
 }
