3a0d524bd9e1281c88760d7a0f5f78aeecf0cd4b
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-20 11:20:59 -0300

    File of fake data added

---
+++ data/albumCategories.ts
@@ -0,0 +1,71 @@
+const albumCategories = [
+  {
+    id: '1',
+    title: 'Pop',
+    albums: [
+      {
+        id: '1',
+        artistsHeadline: 'The Beatles, The Rolling Stones, The Who',
+        imageUri:
+          'https://studiosol-a.akamaihd.net/tb/letras-blog/wp-content/uploads/2020/05/cb26254-capa-abbey-road-1024x1024.jpg',
+      },
+      {
+        id: '2',
+        artistsHeadline: 'Lady Gaga, Ariana Grande, Bruno Mars',
+        imageUri: 'https://brunomarsforever.files.wordpress.com/2012/01/der.jpg',
+      },
+      {
+        id: '3',
+        artistsHeadline: 'Coldplay, Ed Sheeran, The Chainsmokers',
+        imageUri:
+          'https://images-americanas.b2w.io/produtos/125470057/imagens/cd-coldplay-a-head-full-of-dreams/125470065_1_large.jpg',
+      },
+    ],
+  },
+  {
+    id: '2',
+    title: 'Rock',
+    albums: [
+      {
+        id: '1',
+        artistsHeadline: 'Led Zeppelin, Metallica, Queen',
+        imageUri: 'https://wikimetalstore.com.br/produtos/imagens/223_1.jpg',
+      },
+
+      {
+        id: '2',
+        artistsHeadline: 'Metallica, Iron Maiden, Judas Priest',
+        imageUri: 'https://miro.medium.com/max/1400/1*f8nbdubnkJ8i5JSOzYF6VQ.jpeg',
+      },
+      {
+        id: '3',
+        artistsHeadline: 'Queen, Judas Priest, Metallica',
+        imageUri: 'https://www.metal-archives.com/images/2/2/2/2/2222.jpg',
+      },
+    ],
+  },
+  {
+    id: '3',
+    title: 'Chilling',
+    albums: [
+      {
+        id: '1',
+        artistsHeadline: 'Miles Davis, John Coltrane, Duke Ellington',
+        imageUri: 'https://i.pinimg.com/originals/45/ae/dc/45aedc6b5d047e7b57ca38bca2939d68.jpg',
+      },
+      {
+        id: '2',
+        artistsHeadline: 'AURORA, Foster The People, The Weekend',
+        imageUri: 'https://upload.wikimedia.org/wikipedia/pt/a/a4/Aurora_a_different_kind_of_human.jpg',
+      },
+      {
+        id: '3',
+        artistsHeadline: 'Billie Eilish, AURORA',
+        imageUri:
+          'https://s2.glbimg.com/BhvoS48eOJDtKnxOGCIMHMcdKuM=/smart/e.glbimg.com/og/ed/f/original/2021/05/21/billie-eilish-happier-than-ever.jpeg',
+      },
+    ],
+  },
+];
+
+export default albumCategories;
