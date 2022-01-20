414990d9c6bc829e62c6aeb0e613f77b9bbed191
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-20 11:22:50 -0300

    Navigation to AlbumScreen set

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,22 +3,22 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import {FontAwesome5, FontAwesome, Ionicons, Entypo, EvilIcons, MaterialIcons} from '@expo/vector-icons';
+import {FontAwesome5, Entypo, EvilIcons, MaterialIcons} from '@expo/vector-icons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-import {ColorSchemeName, Pressable} from 'react-native';
+import {ColorSchemeName} from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/Home/HomeScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import {RootStackParamList, RootTabParamList, RootTabScreenProps} from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import HomeScreen from '../screens/Home/HomeScreen';
+import AlbumScreen from '../screens/Album/AlbumScreen';
 
 export default function Navigation({colorScheme}: {colorScheme: ColorSchemeName}) {
   return (
@@ -39,6 +39,7 @@ function RootNavigator() {
     <Stack.Navigator>
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{headerShown: false}} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{title: 'Oops!'}} />
+      <Stack.Screen name="Album" component={AlbumScreen} />
       <Stack.Group screenOptions={{presentation: 'modal'}}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
@@ -96,11 +97,4 @@ function BottomTabNavigator() {
       />
     </BottomTab.Navigator>
   );
-}
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {name: React.ComponentProps<typeof FontAwesome>['name']; color: string}) {
-  return <FontAwesome size={30} style={{marginBottom: -3}} {...props} />;
 }
