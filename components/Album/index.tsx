1f23fb45de52fd8f455d5c95401830005f65326c
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-20 11:20:08 -0300

    Styles changes and added the touchableOpacity to index

--- components/Album/index.tsx
+++ components/Album/index.tsx
@@ -1,4 +1,5 @@
-import {Image, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 import {AlbumType} from '../../types';
 import styles from './styles';
 
@@ -6,13 +7,23 @@ export type AlbumProps = {
   album: AlbumType;
 };
 
-const Album = (props: AlbumProps) => (
-  <View style={styles.container}>
-    <Image source={{uri: props.album.imageUri}} style={styles.image} />
-    <Text numberOfLines={2} style={styles.text}>
-      {props.album.artistsHeadline}
-    </Text>
-  </View>
-);
+const Album = (props: AlbumProps) => {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate('Album', {id: props.album.id});
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Image source={{uri: props.album.imageUri}} style={styles.image} />
+        <Text numberOfLines={2} style={styles.text}>
+          {props.album.artistsHeadline}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default Album;
