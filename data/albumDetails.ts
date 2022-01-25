b3ce7a011bd8d35e4552263f814791adb8e81970
BiancaSilvaC <biancascampos0410@gmail.com>
2022-01-25 17:34:45 -0300

    Create albumDetails.ts

---
+++ data/albumDetails.ts
@@ -0,0 +1,50 @@
+const albumDetails = {
+  id: '1',
+  name: 'Good Vibes',
+  by: 'Spotify',
+  numberOfLikes: 587,
+  artistsHeadline: 'The Weeknd, Travis Scott, Foster The People',
+  imageUri: 'https://i.pinimg.com/originals/a1/17/ca/a117ca7d5f44037970cf5a4b7e6a2d43.png',
+  songs: [
+    {
+      id: '1',
+      imageUri: 'https://brunomarsforever.files.wordpress.com/2012/01/der.jpg',
+      title: 'Just the way you are',
+      artist: 'Bruno Mars',
+    },
+    {
+      id: '2',
+      imageUri: 'https://miro.medium.com/max/1400/1*f8nbdubnkJ8i5JSOzYF6VQ.jpeg',
+      title: 'Fear of the dark',
+      artist: 'Iron Maiden',
+    },
+    {
+      id: '3',
+      imageUri: 'https://www.metal-archives.com/images/2/2/2/2/2222.jpg',
+      title: 'Killer Queen',
+      artist: 'Queen',
+    },
+    {
+      id: '4',
+      imageUri:
+        'https://images-americanas.b2w.io/produtos/125470057/imagens/cd-coldplay-a-head-full-of-dreams/125470065_1_large.jpg',
+      title: 'A Head Full of Dreams',
+      artist: 'Coldplay',
+    },
+    {
+      id: '5',
+      imageUri:
+        'https://s2.glbimg.com/BhvoS48eOJDtKnxOGCIMHMcdKuM=/smart/e.glbimg.com/og/ed/f/original/2021/05/21/billie-eilish-happier-than-ever.jpeg',
+      title: 'Happier Than Ever',
+      artist: 'Billie Eilish',
+    },
+    {
+      id: '6',
+      imageUri: 'https://wikimetalstore.com.br/produtos/imagens/223_1.jpg',
+      title: 'Nothing Else Matters',
+      artist: 'Metallica',
+    },
+  ],
+};
+
+export default albumDetails;
